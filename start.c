f6dafdde5f15480224495bac1db9e9bdc1923a55
blossun <dnsngu@gmail.com>
2020-07-22 02:30:59 +0900

    if문 이란

--- start.c
+++ start.c
@@ -1,32 +1,13 @@
+/* if 문 이란? */
 #include <stdio.h>
 
 int main() {
-    /* scanf 포맷 */
-    char ch; //문자
-
-    short sh; //정수
     int i;
-    long lo;
-
-    float fl; //실수
-    double du;
-
-    printf("char 형 변수 입력 : ");
-    scanf("%c", &ch);
-    printf("short 형 변수 입력 : ");
-    scanf("%hd", &sh);
-    printf("int 형 변수 입력 : ");
+    printf("입력하고 싶은 숫자를 입력하세요 : ");
     scanf("%d", &i);
-    printf("long 형 변수 입력 : ");
-    scanf("%ld", &lo);
 
-    printf("float 형 변수 입력 : ");
-    scanf("%f", &fl);
-    printf("double 형 변수 입력 : ");
-    scanf("%lf", &du);
-
-    printf("char : %c, short : %d, int : %d, ", ch, sh, i);
-    printf("long : %ld, float : %f, double : %f \n", lo, fl, du);
-
+    if (i==7) {
+        printf("당신은 행운의 숫자 7을 입력했습니다.");
+    }
     return 0;
 }
