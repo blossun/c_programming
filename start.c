c27211552f5ca9210e0487a269dd1972118db7bf
blossun <dnsngu@gmail.com>
2020-07-22 02:24:22 +0900

    scanf 입력 포맷

--- start.c
+++ start.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    /* scanf : 사용자 입력 받기 */
-    /* 섭씨 온도를 화씨로 바꾸기 */
-    double celsius; //섭씨 온도
+    /* scanf 포맷 */
+    char ch; //문자
 
-    printf("섭씨 온도를 화씨 온도로 바꿔주는 프로그램 입니다. \n");
-    printf("섭씨 온도를 입력해 주세요 : ");
-    scanf("%lf", &celsius); //double형을 입력받을 때는 무조건 %lf
+    short sh; //정수
+    int i;
+    long lo;
 
-    printf("섭씨 %f 도는 화씨로 %f 입니다. \n", celsius, 9 * celsius / 5 + 32);
+    float fl; //실수
+    double du;
+
+    printf("char 형 변수 입력 : ");
+    scanf("%c", &ch);
+    printf("short 형 변수 입력 : ");
+    scanf("%hd", &sh);
+    printf("int 형 변수 입력 : ");
+    scanf("%d", &i);
+    printf("long 형 변수 입력 : ");
+    scanf("%ld", &lo);
+
+    printf("float 형 변수 입력 : ");
+    scanf("%f", &fl);
+    printf("double 형 변수 입력 : ");
+    scanf("%lf", &du);
+
+    printf("char : %c, short : %d, int : %d, ", ch, sh, i);
+    printf("long : %ld, float : %f, double : %f \n", lo, fl, du);
+
     return 0;
 }
