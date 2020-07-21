9d979c45f9ceb954b0db83f91efb0d0e42cd9351
blossun <dnsngu@gmail.com>
2020-07-22 02:36:59 +0900

    크기 비교하기

--- start.c
+++ start.c
@@ -1,15 +1,35 @@
-/* 나눗셈 예제 */
+/* 크기 비교하기 */
 #include <stdio.h>
-
 int main() {
-    double i, j;
-    printf("나누고 싶은 두 정수를 입력하세요 : ");
-    scanf("%lf %lf", &i, &j);
+  int i, j;
 
-    if (j==0) {
-        printf("0으로 나눌 수 없습니다. \n");
-        return 0;
-    }
-    printf("%f를 %f로 나눈 결과는 : %f \n", i, j, i/j);
-    return 0;
+  printf("크기를 비교할 두 수를 입력해 주세요 : ");
+  scanf("%d %d", &i, &j);
+
+  if (i > j)  // i 가 j 보다 크면
+  {
+    printf("%d 는 %d 보다 큽니다 \n", i, j);
+  }
+  if (i < j)  // i 가 j 보다 작으면
+  {
+    printf("%d 는 %d 보다 작습니다 \n", i, j);
+  }
+  if (i >= j)  // i 가 j 보다 크거나 같으면
+  {
+    printf("%d 는 %d 보다 크거나 같습니다 \n", i, j);
+  }
+  if (i <= j)  // i 가 j 보다 작거나 같으면
+  {
+    printf("%d 는 %d 보다 작거나 같습니다 \n", i, j);
+  }
+  if (i == j)  // i 와 j 가 같으면
+  {
+    printf("%d 는 %d 와(과) 같습니다 \n", i, j);
+  }
+  if (i != j)  // i 와 j 가 다르면
+  {
+    printf("%d 는 %d 와(과) 다릅니다 \n", i, j);
+  }
+
+  return 0;
 }
