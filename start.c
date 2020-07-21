3c648944fe0633c8854e14e48089512bc399a42a
blossun <dnsngu@gmail.com>
2020-07-22 02:11:40 +0900

    scanf로 문자를 입력받기

--- start.c
+++ start.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    /* 문자를 저장하는 변수 */
-    char a;
-    a = 'a';
+    /* scanf : 사용자 입력 받기 */
+    /* 섭씨 온도를 화씨로 바꾸기 */
+    double celsius; //섭씨 온도
 
-    printf("a의 값과 들어있는 문자는? 값 : %d, 문자 : %c \n", a, a);
+    printf("섭씨 온도를 화씨 온도로 바꿔주는 프로그램 입니다. \n");
+    printf("섭씨 온도를 입력해 주세요 : ");
+    scanf("%lf", &celsius); //double형을 입력받을 때는 무조건 %lf
 
+    printf("섭씨 %f 도는 화씨로 %f 입니다. \n", celsius, 9 * celsius / 5 + 32);
     return 0;
 }
