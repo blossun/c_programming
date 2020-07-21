12022fc48124eef1e5fa96d01e917806de76e34c
blossun <dnsngu@gmail.com>
2020-07-22 02:04:32 +0900

    문자를 저장하는 변수, 아스키코드

--- start.c
+++ start.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    /* 데이터 크기 */
-    unsigned int b = -1; //-1 은 0xFFFFFFFF 로 표현 ( +1:00000001 -반전-> FFFFFFFE -- +1 --> FFFFFFFF)
-    printf("b 에 들어있는 값을 unsigned int 로 해석했을 때 값 : %u \n", b);
+    /* 문자를 저장하는 변수 */
+    char a;
+    a = 'a';
 
-    b = 4294967295;
-    printf("b : %u \n", b);
+    printf("a의 값과 들어있는 문자는? 값 : %d, 문자 : %c \n", a, a);
 
-    b++;
-    printf("b : %u \n", b);
     return 0;
 }
