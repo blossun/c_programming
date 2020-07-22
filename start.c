58ce02ea8d3a20a66e0aa2c0f5208869d03886d8
blossun <dnsngu@gmail.com>
2020-07-22 12:35:24 +0900

    do-while문 - 1~100합

--- start.c
+++ start.c
@@ -1,12 +1,18 @@
-/* 구구단 */
+/* do-while 1~100합 */
 #include <stdio.h>
 int main() {
-    for (int i=1; i<10; i++) {
-        printf("\n===== %d단 =====\n", i);
-        for (int j=1; j<10; j++) {
-            printf("%d * %d = %d \n", i, j, i*j);
-        }
-    }
+    // int i,sum=0; //둘다 0으로 초기화 안되네...
+    int i=0, sum=0;
+    // int i, sum;
+    // i = sum = 0;
+    printf("i : %d, sum : %d \n", i, sum);
+
+    do {
+        sum += i;
+        i++;
+    }while(i<=100);
+
+    printf("1~100 까지의 합 : %d \n", sum);
 
     return 0;
 }
