a925a9b84d1644b9de8703e25c170ca827d25b2d
blossun <dnsngu@gmail.com>
2020-07-22 14:18:13 +0900

    피보나치 수열 구하기

--- start.c
+++ start.c
@@ -1,16 +1,22 @@
-/* 문제3 */
-// 1000 이하의 3 또는 5 의 배수인 자연수들의 합을 구한다.
+/* 문제4 */
+// 1000000 이하의 피보나치 수열 ( N 번째 항이 N - 1 번째 항과 N - 2 번째 항으로 표현되는 수열, 시작은 1,1,2,3,5,8,...) 의 짝수 항들의 합을 구한다
 #include <stdio.h>
-int main() {
-    int sum=0;
 
-    for (int i=1; i<=1000; i++) {
-        if (i%3==0 || i%5==0) {
-            sum += i;
-        }
+int pibo(int n) {
+    if (n==1 || n==2) {
+        return 1;
     }
+    return pibo(n-1) + pibo(n-2);
+}
 
-    printf("1000 이하의 3 또는 5 의 배수인 자연수들의 합 : %d\n", sum);
+int main() {
+    int sum = 0;
+    
+    for (int i=1; i<=10; i++) {
+        int number = pibo(i);
+        printf("%d, ", number);
+        sum += number;
+    }
 
     return 0;
 }
