8931e03ad88bd8d2270985182bc384f9c542759f
blossun <dnsngu@gmail.com>
2020-07-22 21:44:07 +0900

    배열의 크기를 변수로 지정

--- start.c
+++ start.c
@@ -1,28 +1,30 @@
-/* 배열 */
-// 각 학생들의 성적들을 입력받아서 평균 보다 낮은 사람들의 번호 옆에 '불합격', 
-// 평균 이상의 사람들에게 '합격' 이라는 메세지 까지 출력
+/* 과연 될까? */
 #include <stdio.h>
 int main() {
-    int score[10];
-    int sum = 0;
-    int avg;
+  int total;
+  printf("전체 학생수 : ");
+  scanf("%d", &total);
+  int arr[total];
+  int i, ave = 0;
 
-    for (int i=0; i<10; i++) {
-        printf("%d번쨰 학생 점수 : ", i+1);
-        scanf("%d", &score[i]); //&(score[i]) 라고 안해도 된다.
-        sum += score[i];
-    }
+  for (i = 0; i < total; i++) {
+    printf("%d 번째 학생의 성적은? ", i + 1);
+    scanf("%d", &arr[i]);
+  }
+  for (i = 0; i < total; i++) {
+    ave = ave + arr[i];
+  }
 
-    avg = sum / 10; //size를 따로 구할 수 없음...?
+  ave = ave / total;
+  printf("전체 학생의 평균은 : %d \n", ave);
 
-    for (int i=0; i<10; i++) {
-        printf("학생 %d : ", i+1);
-        if (score[i] >= avg) {
-            printf("합격 \n");
-        } else {
-            printf("불합격 \n");
-        }
-    }
+  for (i = 0; i < total; i++) {
+    printf("학생 %d : ", i + 1);
+    if (arr[i] >= ave)
+      printf("합격 \n");
+    else
+      printf("불합격 \n");
+  }
 
-    return 0;
+  return 0;
 }
