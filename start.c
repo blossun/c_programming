c3c4ad67d84e32e15a653496968381c861cea3af
blossun <dnsngu@gmail.com>
2020-07-22 23:39:08 +0900

    &연산자

--- start.c
+++ start.c
@@ -1,16 +1,13 @@
-/* 2차원 배열 */
+/* & 연산자, 포인터의 시작 */
 #include <stdio.h>
 int main() {
-    // int arr[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; //위와 동일
+    int* p;
+    int a=2;
 
-    for (int i=0; i<3; i++) {
-        printf("[%d]행 : ", i);
-        for (int j=0; j<3; j++) {
-            printf("%d", arr[i][j]);
-        }
-        printf("\n");
-    }
+    p = &a;
 
-  return 0;
+    printf("포인터 p에 들어있는 값 : %p \n", p); //%p : 16진수로 출력
+    printf("int 변수 a가 저장된 주소값 : %p \n", &a);
+
+    return 0;
 }
