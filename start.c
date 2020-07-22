2d41d2b12f8b77583ef1759cc427dc77d290d189
blossun <dnsngu@gmail.com>
2020-07-22 12:22:21 +0900

    2중 for문 - 구구단

--- start.c
+++ start.c
@@ -1,11 +1,11 @@
-/* continue */
+/* 구구단 */
 #include <stdio.h>
 int main() {
-    int i;
-
-    for (i=0; i<100; i++) {
-        if (i%5==0) continue;
-        printf("%d ", i);
+    for (int i=1; i<10; i++) {
+        printf("\n===== %d단 =====\n", i);
+        for (int j=1; j<10; j++) {
+            printf("%d * %d = %d \n", i, j, i*j);
+        }
     }
 
     return 0;
