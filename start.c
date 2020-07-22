db658793b07c2c0386e6885312fd681004b37dc7
blossun <dnsngu@gmail.com>
2020-07-22 19:14:19 +0900

    형변환

--- start.c
+++ start.c
@@ -1,31 +1,15 @@
-/* switch 문자 비교 */
-// 컴퓨터는 문자를 모두 숫자로 처리한 뒤, 우리에게 보여줄 때 에만 문자로 보여주는 것
 #include <stdio.h>
 int main() {
-  char input;
+  int a,b;
+  float c,d;
 
-  printf("(소문자) 알파벳 읽기\n");
-  printf("알파벳 : ");
+  a = 5;
+  b = 3;
 
-  scanf("%c", &input);
+  c = a / b;
+  d = (float)a / b;
 
-  switch (input) {
-    case 97: //case 'a': 와 같다.
-      printf("에이 \n");
-      break;
-
-    case 'b':
-      printf("비 \n");
-      break;
-
-    case 'c':
-      printf("씨 \n");
-      break;
-
-    default:
-      printf("죄송해요.. 머리가 나빠서 못 읽어요  \n");
-      break;
-  }
+  printf("두 수의 비율 : %f %f", c, d); //두 수의 비율 : 1.000000 1.666667
 
   return 0;
 }
