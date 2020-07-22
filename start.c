4e48f78a891d5f0b7c16da69dacad0c15d11decf
blossun <dnsngu@gmail.com>
2020-07-22 12:10:03 +0900

    반복문 - 과목 평균 구하기

--- start.c
+++ start.c
@@ -1,25 +1,20 @@
-/* 논리 연산자 */
+/* 과목 평균 구하기 */
 #include <stdio.h>
 int main() {
-    int a;
-    printf("숫자 입력 : ");
-    scanf("%d", &a);
+    int i;
+    int subject, score;
+    double totalScore;
 
-    if (a>=10 && a<20) { //논리합
-        printf("%d는 10이상 20미만인 수\n", a);
+    printf("과목 수 입력 : ");
+    scanf("%d", &subject);
+
+    for (i=0; i<subject; i++) {
+        printf("과목%d : ", i+1);
+        scanf("%d", &score);
+        totalScore += score;
     }
 
-    int height, weight;
-    printf("당신의 키와 몸무게를 각각 입력해 주세요 : ");
-    scanf("%d %d", &height, &weight);
-
-    if (height >= 190 || weight >= 100) { //논리곱
-        printf("당신은 '거구' 입니다. \n");
-    }
-
-    if (!(height >= 190 || weight >= 100)) { //논리부정
-        printf("당신은 거구가 아닙니다. \n");
-    }
+    printf("평균 : %.2f \n", totalScore/subject);
 
     return 0;
 }
