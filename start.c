86a12a6e255f138623dbe65e4b5f7aa5bca91f93
blossun <dnsngu@gmail.com>
2020-07-22 22:14:28 +0900

    2차원 배열

--- start.c
+++ start.c
@@ -1,8 +1,16 @@
-/* 초기화 되지 않은 값 */
+/* 2차원 배열 */
 #include <stdio.h>
 int main() {
-  int arr[3] = {1};
-  printf("니 값은 모니 : %d", arr[1]);  // arr[0] 이 아닌 arr[1] 을 출력
+    // int arr[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; //위와 동일
+
+    for (int i=0; i<3; i++) {
+        printf("[%d]행 : ", i);
+        for (int j=0; j<3; j++) {
+            printf("%d", arr[i][j]);
+        }
+        printf("\n");
+    }
 
   return 0;
 }
