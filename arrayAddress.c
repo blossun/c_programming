2b3e457824350a75438934bcca889bc27a1f00d1
blossun <dnsngu@gmail.com>
2020-07-23 13:47:52 +0900

    신기한 [] 연산

--- arrayAddress.c
+++ arrayAddress.c
@@ -4,8 +4,8 @@
 int main() {
     int arr[5] = {1, 2, 3, 4, 5};
 
-    printf("a[3] : %d \n", arr[3]);
-    printf("*(arr+3) : %d \n", *(arr+3));
+    printf("3[arr] : %d \n", 3[arr]);
+    printf("*(3+arr) : %d \n", *(3+arr));
 
     return 0;
 }
