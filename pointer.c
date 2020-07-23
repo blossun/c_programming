e82ef2ac5f09138d14bc81fb48c8e62393a73f19
blossun <dnsngu@gmail.com>
2020-07-23 15:57:34 +0900

    포인터의 형(type) 을 결정짓는 두 가지 요소
    1. 가리키는 것에 대한 정보 (예를 들어, `int*` 이면 `int` 를 가리킨다, `char**` 이면 `char*` 을 가리킨다 등등)
    2. 1 증가시 커지는 크기 (2 차원 배열에서는 `b * (형의 크기)` 를 의미한다 1 증가시 커지는 크기 (2 차원 배열에서는 `b * (형의 크기)` 를 의미한다)

--- pointer.c
+++ pointer.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 
 int main() {
-    int arr[2][3];
+    int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
 
-    printf("arr[0] : %p \n", arr[0]);
-    printf("&arr[0][0] : %p \n", &arr[0][0]);
+    printf("arr : %p , arr + 1 : %p \n", arr, arr + 1);
+    
+    //int *parr = &arr; // 열의 갯수를 알아야 주소 연산을 할 수 있음. 따라서 컴파일에러가 난다.
+    int (*parr)[3]; //가르키는 2차원 배열의 열의 크기를 지정해줘야 한다.
+    
+    parr = arr; // parr 이 arr 을 가리키게 한다.
 
-    printf("arr[1] : %p \n", arr[1]);
-    printf("&arr[1][0] : %p \n", &arr[1][0]);
- 
+    printf("arr[1][0] : %d , *(arr + 1) : %d \n", arr[1][0], *parr[1]);
+
+    printf("arr[1][2] : %d, parr[1][2] : %d, *parr[1]+2 : %d \n", arr[1][2], parr[1][2], *parr[1]+2);
+    //printf("*parr[1][2] : %d \n", *parr[1][2]); //오류
+
     return 0;
 }
