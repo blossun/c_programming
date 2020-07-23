0309c053b323f2048e7c13cee141fe6fca34c658
blossun <dnsngu@gmail.com>
2020-07-23 13:40:14 +0900

    배열과 포인터
    배열의 이름이 `sizeof` 연산자나 주소값 연산자(`&`)와 사용될 때 (예를 들어 `&arr`) 경우를 빼면, 배열의 이름을 사용시 암묵적으로 첫 번째 원소를 가리키는 포인터로 타입 변환된다.

--- arrayAddress.c
+++ arrayAddress.c
@@ -2,25 +2,11 @@
 #include <stdio.h>
 
 int main() {
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int* parr;
-    int i;
-    parr = &arr[0];
+    int arr[3] = {1, 2, 3};
+    int* parr = arr;
 
-    for (int i=0; i<10; i++) {
-        printf("arr[%d] 의 주소값 : %p \n", i, &arr[i]);
-        printf("(parr + %d) 의 주소값 : %p \n", i, (parr+i));
-
-        if (&arr[i] == (parr+i)) {
-            /* 만일 (parr + i) 가 성공적으로 arr[i] 를 가리킨다면 */
-            printf(" --> 일치 \n");
-        } else {
-            printf(" --> 불일치 \n");
-        }
-    }
-
-    // 포인터로 배열의 인덱스에 접근하는 방법
-    printf("arr[3] = %d, *(parr+3) = %d \n", arr[3], *(parr+3));
+    printf("sizeof(arr) : %lu \n", sizeof(arr));
+    printf("sizeof(parr) : %lu \n", sizeof(parr));
 
     return 0;
 }
