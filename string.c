ad36c575c04ba44b256a22c515ad37af3e98f050
blossun <dnsngu@gmail.com>
2020-07-26 00:31:04 +0900

    구조체 예제 2
    구조체 내에서 변수 초기화 불가능

--- string.c
+++ string.c
@@ -1,23 +1,42 @@
-/* 구조체 */
+/* 구조체 예제 2 */
 #include <stdio.h>
 
-struct Human {
-    int age;
-    int height;
-    int weight;
+char copy_str(char *dest, char *src);
+struct Books {
+    /* 책이름 */
+    char name[30];
+    /* 작가 */
+    char auth[30];
+    /* 출판사 */
+    char publ[30];
+    /* 대여 여부 */
+    int borrowed;
+    // int borrowed=0; //에러. 구조체에서 변수 초기화 불가능
 };
 
 int main() {
-    struct Human Holar;
+    struct Books Harry_Potter;
 
-    Holar.age = 99;
-    Holar.height = 170;
-    Holar.weight = 55;
+    copy_str(Harry_Potter.name, "Harry Potter");
+    copy_str(Harry_Potter.auth, "J.K Rooling");
+    copy_str(Harry_Potter.publ, "Scholastic");
+    Harry_Potter.borrowed = 0;
 
-    printf("Holar에 대한 정보 \n");
-    printf("나이 : %d \n", Holar.age);
-    printf("키  : %d \n", Holar.height);
-    printf("몸무게 : %d \n", Holar.weight);
+    printf("책 이름 : %s \n", Harry_Potter.name);
+    printf("저자 : %s \n", Harry_Potter.auth);
+    printf("출판사 : %s \n", Harry_Potter.publ);
 
   return 0;
+}
+
+char copy_str(char *dest, char *src) {
+    while(*src) {
+        *dest = *src;
+        src++;
+        dest++;
+    }
+
+    *dest = '\0';
+
+    return 1;
 }
