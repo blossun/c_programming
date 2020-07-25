626711572ff700b3360281584e8f260d5b6140fb
blossun <dnsngu@gmail.com>
2020-07-25 20:43:52 +0900

    리터럴 - 수정하려고 하면 에러 발생

--- string.c
+++ string.c
@@ -1,17 +1,12 @@
-/* c 에 무엇이 들어가는지 살짝 보아야 하므로 코드를 약간 수정했습니다 */
+/* 문자열 */
 #include <stdio.h>
+
 int main() {
-  int num, i;
-  char c;
+  char str[] = "hello";
+  char *pstr = "goodbye";
 
-  printf("숫자를 입력하세요 : ");
-  scanf("%d", &num);
+  str[1] = 'a';
+//   pstr[1] = 'a'; //디버깅하면 여기서 에러나는 것 확인가능. 리터럴값을 수정하려고 하기 때문
 
-  getchar();
-
-  printf("문자를 입력하세요 : ");
-  scanf("%c", &c);
-
-  printf("입력한 문자 : %c", c);
   return 0;
 }
