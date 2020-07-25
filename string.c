66e2f166a905b857de7645e093745452af7a25af
blossun <dnsngu@gmail.com>
2020-07-25 17:41:27 +0900

    공백문자가 버퍼에 남아있을 때 scanf로 %s를 입력받기

--- string.c
+++ string.c
@@ -1,15 +1,15 @@
-/* 이상한 scanf */
+/* 이상한 scanf - %s는 ?? */
 #include <stdio.h>
 
 int main() {
     int num;
-    char c;
+    char str[30];
 
     printf("숫자를 입력하세요 : ");
     scanf("%d", &num);
 
-    printf("문자를 입력하세요 : ");
-    scanf("%c", &c);
+    printf("문자열을 입력하세요 : ");
+    scanf("%s", str); 
 
     return 0;
 }
