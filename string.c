083c9ecc5cfc8d597d66fd81ecf4772c19b65577
blossun <dnsngu@gmail.com>
2020-07-25 16:35:32 +0900

    문자열일력 - 띄어쓰기 문제

--- string.c
+++ string.c
@@ -1,25 +1,13 @@
-/* 문자열과 포인터*/
+/* 문자열 입력*/
 #include <stdio.h>
 
-int size_of_string(char word[]);
 int main() {
-    char word[30] = {"long sentence"};
-    char *str = word;
+   char word[30]; //29자 이하의 문자열을 입력받을 수 있는 데이터
 
-    printf("%s \n", str);
-    printf("문자열 길이 : %d \n", size_of_string(str));
+   printf("30자 이내의 문자열 입력 : ");
+   scanf("%s", word); // &연산자 필요없음. 배열의 이름 자체가 배열을 가리키고 있는 포인터
+
+   printf("문자열 : %s \n", word);
 
     return 0;
-}
-
-// int size_of_string(char *word) {
-int size_of_string(char word[]) {
-    int i=0;
-
-    while (word[i]) { //str[i] 가 0 이 될 때 까지 i 의 값을 계속 증가
-        i++;
-    }
-
-    //str[i]가 0이면 null을 만난 것
-    return i; //i : 맨 마지막의 NULL 문자를 제외한 나머지 문자들의 총 개수
 }
